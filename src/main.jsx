efb359faace4eac4a845deb26e63fea1b794ac54
Dafina2001 <da52811@ubt-uni.net>
2024-02-17 10:43:20 -1100

    Router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import About from './Components/About/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>, 
+      },
+      {
+        path: "/about",
+        element: <About/>, 
+      }
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+import SectionHome from './Section-Home'
+
+export default function Home (){
+    return (
+        <>
+        <div className="Home">
+            <SectionHome/>
+        </div>
+        
+        </>
+    )
+}
