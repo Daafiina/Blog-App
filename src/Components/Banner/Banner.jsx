e99f8cdc23d573832ccbfd8e8d3723172e36d4e6
Dafina2001 <da52811@ubt-uni.net>
2024-02-20 03:50:06 -1100

    Some home style

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Banner() {
-    return (
-      <div className='px-4 py-32 bg-slate-500 mx-auto '>
-        <div className="text-white text-center">
-          <h1 className='text-6xl lg:text-7x1 leading-snug font-bold mb-5'>Welcome to Our Blog</h1>
-          <p className="text-gray-100 text-2xl lg:w-3/5 mx-auto mb-5 font-primary">Start your blog today and join a community of writers and readers who are passionate about
-            sharing theri stories and ideas.We offer everything you need to get started,from helpful tips and
-            tutorials
-          </p>
-          <div>
-            <Link to="/about" className='font-medium hover:text-blue-400'>Learn more</Link>
-          </div>
+  return (
+    <div className='px-4 py-32 bg-slate-500 mx-auto'>
+      <div className="text-white text-center transition duration-500 ease-in-out transform hover:scale-105">
+        <h1 className='text-6xl lg:text-7xl leading-snug font-bold mb-5'>Welcome to Our Blog</h1>
+        <p className="text-gray-100 text-2xl lg:w-3/5 mx-auto mb-5 font-primary transition duration-500 ease-in-out transform hover:scale-105">Start your blog today and join a community of writers and readers who are passionate about sharing their stories and ideas. We offer everything you need to get started, from helpful tips and tutorials.</p>
+        <div>
+          <Link to="/about" className='font-medium hover:text-blue-400 transition duration-300 ease-in-out transform hover:scale-105'>Learn more</Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-export default Banner
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import Banner from '../Components/Banner/Banner'
+import BlogList from '../Components/Blogs/BlogList'
 import './Home.css'
-import SectionHome from './Section-Home'
+import {useEffect} from 'react' 
+import { useHistory } from 'react-router-dom';
 
 export default function Home (){
+    const history=useHistory();
+    useEffect(() => {
+    let username=sessionStorage.getItem('username');
+    if(username === '' || username===null){
+        history.push('/login')
+    }
+    },[]);
     return (
         <>
         <Banner/>
+        <BlogList/>
+        
             {/* <SectionHome/> */}
         
         
