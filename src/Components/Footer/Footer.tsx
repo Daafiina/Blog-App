d398395f4aeafca350891627ba7043e01ebede67
Dafina2001 <da52811@ubt-uni.net>
2024-02-21 03:59:49 -1100

    Footer Added

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {  ClipboardDocumentListIcon, XMarkIcon } from '@heroicons/react/24/solid';
+
+function Footer() {
+  return (
+   <>
+   </>
+  );
+}
+
+export default Footer;
